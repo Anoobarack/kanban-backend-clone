c16e07a67b4d1bb7466f2cdabaad1cb2a7e82f2c
Игорь Головко <igor-golovko-99@mail.ru>
2026-05-10 16:33:30 +0700

    github actions test

--- src/main/java/com/wkrzywiec/medium/kanban/KanbanApplication.java
+++ src/main/java/com/wkrzywiec/medium/kanban/KanbanApplication.java
@@ -12,6 +12,7 @@ public class KanbanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanApplication.class, args);
+		System.out.println("Backend CI/CD demo build is running");
 	}
 
 }
